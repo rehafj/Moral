2351070673aed17f0b9a7e8143cb6d161c946b13
rehafj <rj.codesstuff@gmail.com>
2020-11-10 01:02:53 -0800

    sample json in
    update

--- New Unity Project/Assets/scripts/CharacterTrigger.cs
+++ New Unity Project/Assets/scripts/CharacterTrigger.cs
@@ -1,20 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Fungus;
 
 public class CharacterTrigger : MonoBehaviour
 {
     [SerializeField]
-    public Flowchart TomFlowChar;
     public Character character;
     public Dialoug dialog;
 
     public void Start()
     {
-        Block block = new Block();
         //block.CommandList.Add(sayco);
-        TomFlowChar.AddSelectedBlock(block);
 
     }
 
